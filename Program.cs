723864f9990e4c4da224f711e40baa7efff6efb0
bikfordov-shnur <35244474+bikfordov-shnur@users.noreply.github.com>
2018-01-09 08:08:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TasukePad
+{
+    class Program
+    {
+        static string[] keywords = new string[] { "content", "english", "japanese", "essence" };
+
+        public static void Main()
+        {
+            ChoosePath.ChooseRightPath();
+            Input.AddNewElements();
+            if (keywords[0].Equals(Console.ReadLine()))
+            {
+                Output.ReadFromFileAndPrint();
+            }
+            else
+            {
+                Console.WriteLine("     !Incorrect keyword...");
+                Console.Read();
+            }
+        }
+    }
+}
